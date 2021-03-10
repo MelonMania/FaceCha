43c3e990b7c9b55b1ac8b8ca87d581073a4be0d6
MelonMania <dnwlsskfn@gmail.com>
2021-03-11 07:27:42 +0900

    Updating Design

--- FaceCha/Control/ViewController.swift
+++ FaceCha/Control/ViewController.swift
@@ -11,17 +11,25 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var uploadedImage: UIImageView!
     @IBOutlet weak var celebrityName: UILabel!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var selectButton: UIButton!
     
     var imageManager = ImageManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageManager.delegate = self
+        
+        searchButton.layer.cornerRadius = 10
+        selectButton.layer.cornerRadius = 20
         uploadedImage.image = UIImage(named: "wj.jpeg")
     }
     
     @IBAction func pressSearch(_ sender: UIButton) {
         imageManager.postData()
+    }
+    
+    @IBAction func pressSelect(_ sender: UIButton) {
     }
     
 }
